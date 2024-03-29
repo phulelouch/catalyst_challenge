39884ea977a9b1f330e8fe3f3405f30ca8a0a631
phulelouch <phulelouch@gmail.com>
2024-03-30 00:07:41 +1100

    Type: Feature. Scope: user_upload.php. Message: Test connection with database

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 if [ "$1" = '/bin/bash' ]; then
-    service mysql start
+	service mysql start
+	#This is for test connection will remove later
+	mysql -e "CREATE DATABASE IF NOT EXISTS users;"
+	mysql -e "CREATE USER IF NOT EXISTS 'phulelouch'@'localhost' IDENTIFIED BY 'phulelouch';"
+	mysql -e "GRANT ALL PRIVILEGES ON users.* TO 'phulelouch'@'localhost';"
+	mysql -e "FLUSH PRIVILEGES;"
 	exec "$@"
 else
 	service mysql start
 
-	#mysql -e "CREATE DATABASE IF NOT EXISTS mydatabase;"
+	#mysql -e "CREATE DATABASE IF NOT EXISTS users;"
 	#mysql -e "CREATE USER IF NOT EXISTS 'phulelouch'@'localhost' IDENTIFIED BY 'phulelouch';"
-	#mysql -e "GRANT ALL PRIVILEGES ON mydatabase.* TO 'phulelouch'@'localhost';"
+	#mysql -e "GRANT ALL PRIVILEGES ON users.* TO 'phulelouch'@'localhost';"
 	#mysql -e "FLUSH PRIVILEGES;"
 
 	php /usr/src/app/user_upload.php "$@"
--- user_upload.php
+++ user_upload.php
@@ -1,3 +1,19 @@
 <?php
-  phpinfo();
+  // Test connection
+  $options = getopt("u:p:h:");
+  $host = $options["h"] ?? 'localhost'; 
+  $username = $options["u"] ?? 'root'; 
+  $password = $options["p"] ?? ''; 
+  $database = 'users'; 
+
+  $conn = new mysqli($host, $username, $password, $database);
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } else {
+      echo "\nSuccess\n";
+  }
+
+
+  $conn->close();
 ?>
